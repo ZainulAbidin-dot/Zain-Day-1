e57e3e0acfe54de5c96ae06d7714e8124d5f06ba
ZainulAbidin-dot <zchxvr@gmail.com>
2022-06-20 15:24:50 +0500

    First

---
+++ main.js
@@ -0,0 +1,14 @@
+// // Install Math.js library to run this code
+// // To install Math.js type: npm i mathjs 
+
+// Euler's formula
+var e = math.e;
+var pi = math.pi;
+var i = math.i;
+var num = math.im(i)*pi;
+var euler = math.pow(e,num)+1;
+
+// console.log(e);
+// console.log(pi);
+// console.log(i);
+console.log(euler);
